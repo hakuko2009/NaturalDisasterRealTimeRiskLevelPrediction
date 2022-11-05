47a8195c2e106616e1353e1d337065f22b63123f
Nguyen Le Thien Nhu <nguyenlethiennhu1303@gmail.com>
2022-11-06 01:12:37 +0700

    Refactor code

--- DataLoader.py
+++ DataLoader.py
@@ -1,9 +1,11 @@
 import csv
-import os
+import os.path
+
+import pandas as pd
 
 import DataConvert
 
-source_file = 'data/emdat_public_2022_09_24.csv'
+source_file = 'data/emdat_public_2022_10_18.csv'
 
 
 def get_case_list():
@@ -11,35 +13,31 @@ def get_case_list():
     caseDict = get_data_from_csv()
     print('Done get data')
 
-    level_0 = list()
-    level_1 = list()
-    level_2 = list()
-    level_3 = list()
+    level = list()
+    featureList = list()
+    resultList = list()
 
     for disNo in caseDict:
         features = caseDict[disNo]
-        case = (disNo, features[0], features[1], features[2],
+        case = (features[0], features[1], features[2],
                 features[3], features[4], features[5], features[6])
-        print('case = ', case)
-        calculatedRiskLevel = features[6]
-        if calculatedRiskLevel == 0:
-            level_0.append(case)
-        elif calculatedRiskLevel == 1:
-            level_1.append(case)
-        elif calculatedRiskLevel == 2:
-            level_2.append(case)
-        else:
-            level_3.append(case)
+        case = pd.get_dummies(case)
+        calculatedRiskLevel = features[7]
+        featureList.append(case)
+        resultList.append(calculatedRiskLevel)
 
-    return level_0, level_1, level_2, level_3
+        level.append(case)
+
+    return featureList, resultList
 
 
 def get_data_from_csv():
     # Convert csv data to dictionary
-    # DataConvert.cleanAndConvert()
+    if not (os.path.exists(source_file)):
+        DataConvert.cleanAndConvert()
 
-    path = os.path.abspath(__file__)
-    dir_path = os.path.dirname(path) + '/'
+    filePath = os.path.abspath(__file__)
+    dir_path = os.path.dirname(filePath) + '/'
 
     names = dict()
     with open(dir_path + '/' + source_file, 'r', newline='', encoding='utf8') as csv_file:
@@ -47,13 +45,15 @@ def get_data_from_csv():
         for row in reader:
             # Skip case with missing value
             if str(row['Calculated Risk Level']) == '' or str(row['Year']) == 'None' \
-                    or str(row['Disaster Subtype']) == 'None' or str(row['Country']) == 'None' \
-                    or str(row['Region']) == 'None' or str(row['Start Month']) == '' \
+                    or str(row['Disaster Type']) == 'None' or str(row['Region']) == 'None' \
+                    or str(row['Dis Mag Value']) == '' or str(row['Dis Mag Scale']) == 'None' \
+                    or str(row['Start Month']) == '' \
                     or str(row['End Month']) == '':
                 continue
 
-            names[row['Dis No']] = [int(row['Year']), str(row['Disaster Subtype']), str(row['Country']),
-                                    str(row['Region']), int(float(row['Start Month'])), int(float(row['End Month'])),
+            names[row['Dis No']] = [int(float(row['Year'])), str(row['Disaster Type']), str(row['Region']),
+                                    int(float(row['Dis Mag Value'])), str(row['Dis Mag Scale']),
+                                    int(float(row['Start Month'])), int(float(row['End Month'])),
                                     int(float(row['Calculated Risk Level']))]
 
     return names
--- LevelClassifier.py
+++ LevelClassifier.py
@@ -1,109 +1,131 @@
-from nltk import NaiveBayesClassifier, classify
+import os
+import pickle
+
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+from fastapi import FastAPI, Form
+from starlette.responses import HTMLResponse
+
+import DataConvert
 import DataLoader
-import random
-import model.CaseOfDisaster as Case
-from datetime import datetime
+
+source_file = 'data/emdat_public_2022_10_18.csv'
+app = FastAPI()
+
+
+@app.get('/')
+def basic_view():
+    return {"WELCOME": "GO TO /docs route, or /post or send post request to /predict "}
+
+
+@app.get('/predict', response_class=HTMLResponse)
+def take_inp():
+    return '''<form method="post">
+            <input maxlength="28" name="text" type="text" value="Text Emotion to be tested" />
+            <input type="submit" />'''
+
+
+@app.post('/predict')
+def predict():
+    year = 2022
+    type = "Drought"
+    region = "South-Eastern Asia"
+    magValue = 3000
+    magScale = "Kph"
+    startMonth = 9
+    endMonth = 9
+
+    detectionFile = open('detection.h5', 'rb')
+    loadedModel = pickle.load(detectionFile)
+    detectionFile.close()
+
+    typeFile = open('TypeEncoder.pickle', 'rb')
+    typeEn = pickle.load(typeFile)
+    typeFile.close()
+
+    regionFile = open('RegionEncoder.pickle', 'rb')
+    regionEn = pickle.load(regionFile)
+    regionFile.close()
+
+    magScaleFile = open('MagScaleEncoder.pickle', 'rb')
+    magScaleEn = pickle.load(magScaleFile)
+    magScaleFile.close()
+
+    encodedType = typeEn.transform([type])
+    encodedRegion = regionEn.transform([region])
+    encodedMagScale = magScaleEn.transform([magScale])
+
+    caseDict = [[year, encodedType[0], encodedRegion[0], magValue,
+                 encodedMagScale[0], startMonth, endMonth]]
+
+    print(caseDict)
+    result = loadedModel.predict(caseDict)[0]
+    return {result}
 
 
 def _case_features(caseFeatures):
     return {
         'year': caseFeatures[1],
-        'subtype': caseFeatures[2],
-        'country': caseFeatures[3],
-        'region': caseFeatures[4],
-        'startMonth': caseFeatures[5],
-        'endMonth': caseFeatures[6]
+        'type': caseFeatures[2],
+        'region': caseFeatures[3],
+        'magValue': caseFeatures[4],
+        'magScale': caseFeatures[5],
+        'startMonth': caseFeatures[6],
+        'endMonth': caseFeatures[7]
     }
 
 
 class LevelClassifier:
+    def __init__(self):
+        pass
+
     def get_features(self):
         # classify into 4 level (0 -> 3)
-        level_0, level_1, level_2, level_3 = DataLoader.get_case_list()
 
-        feature_set = list()
-
-        for cases in level_0:
-            features = _case_features(cases)
-            features['level_0'] = 1.0
-            features['level_1'] = 0.0
-            features['level_2'] = 0.0
-            features['level_3'] = 0.0
-            feature_set.append((features, 'L0'))
-
-        for cases in level_1:
-            features = _case_features(cases)
-            features['level_0'] = 0.0
-            features['level_1'] = 1.0
-            features['level_2'] = 0.0
-            features['level_3'] = 0.0
-            feature_set.append((features, 'L1'))
-
-        for cases in level_2:
-            features = _case_features(cases)
-            features['level_0'] = 0.0
-            features['level_1'] = 0.0
-            features['level_2'] = 1.0
-            features['level_3'] = 0.0
-            feature_set.append((features, 'L2'))
-
-        for cases in level_3:
-            features = _case_features(cases)
-            features['level_0'] = 0.0
-            features['level_1'] = 0.0
-            features['level_2'] = 0.0
-            features['level_3'] = 1.0
-            feature_set.append((features, 'L3'))
+        feature_set = DataLoader.get_case_list()
 
         return feature_set
 
-    def train_and_test(self, training_percent=0.80):
-        feature_set = self.get_features()
-        random.shuffle(feature_set)
+    def train_and_test(self):
+        if not (os.path.exists(source_file)):
+            DataConvert.cleanAndConvert()
 
-        case_count = len(feature_set)
+        data = pd.read_csv(source_file, encoding='utf8')
+        data = data[
+            ['Dis No', 'Year', 'Disaster Type', 'Region', 'Dis Mag Value', 'Dis Mag Scale', 'Start Month',
+             'End Month', 'Calculated Risk Level']]
 
-        cut_point = int(case_count * training_percent)
+        labelEncoder = preprocessing.LabelEncoder()
+        typeEncoder = preprocessing.LabelEncoder()
+        regionEncoder = preprocessing.LabelEncoder()
+        magScaleEncoder = preprocessing.LabelEncoder()
 
-        train_set = feature_set[:cut_point]
-        test_set = feature_set[cut_point:]
+        data['Disaster Type'] = typeEncoder.fit_transform(data['Disaster Type'])
+        data['Region'] = regionEncoder.fit_transform(data['Region'])
+        data['Dis Mag Scale'] = magScaleEncoder.fit_transform(data['Dis Mag Scale'])
 
-        self.train(train_set)
+        pickle.dump(typeEncoder, open('TypeEncoder.pickle', 'wb'))
+        pickle.dump(regionEncoder, open('RegionEncoder.pickle', 'wb'))
+        pickle.dump(magScaleEncoder, open('MagScaleEncoder.pickle', 'wb'))
 
-        return self.test(test_set)
+        X = data[['Year', 'Disaster Type', 'Region', 'Dis Mag Value', 'Dis Mag Scale', 'Start Month',
+                  'End Month']]
+        Y = labelEncoder.fit_transform(data['Calculated Risk Level'])
 
-    def classify(self, case):
-        features = _case_features(case)
-        return self.classifier.classify(features)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
+        print(X_train)
+        print(Y_train)
 
-    def train(self, train_set):
-        self.classifier = NaiveBayesClassifier.train(train_set)
-        return self.classifier
-
-    def test(self, test_set):
-        return classify.accuracy(self.classifier, test_set)
-
-    def get_most_informative_features(self, n=5):
-        return self.classifier.most_informative_features(n)
+        naiveBayesModel = BernoulliNB()
+        naiveBayesModel.fit(X_train, Y_train)
+        file = open('detection.h5', 'wb')
+        pickle.dump(naiveBayesModel, file)
 
 
 if __name__ == "__main__":
     gp = LevelClassifier()
-    accuracy = gp.train_and_test()
-    print('Accuracy: %f' % accuracy)
-    print('Most Informative Features')
-    feats = gp.get_most_informative_features(10)
-    # get current datetime as index of case
-    disNo = datetime.now().strftime("%m/%d/%Y-%H:%M:%S")
+    gp.train_and_test()
 
-    print("Enter an instance of disaster case: ")
-    year = int(input('Year: '))
-    subtype = str(input('Subtype of disaster: '))
-    country = str(input('Country: '))
-    region = str(input('Region: '))
-    startMonth = int(input('Start Month: '))
-    endMonth = int(input('End Month (presumed): '))
-    case = Case.CaseOfDisaster(disNo=disNo, year=year, subtype=subtype, country=country,
-                               region=region, startMonth=startMonth, endMonth=endMonth)
-
-    print('\n%s is classified as %s' % (case, gp.classify(case)))
+    # print('\n%s is classified as %s' % (case, gp.classify(case)))
